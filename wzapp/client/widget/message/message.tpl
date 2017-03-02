a2c998d2af3f60fb48250aeebb0f45aa62da15d8
245160801@qq.com <245160801@qq.com>
2017-03-02 19:18:01 +0800

    自定义 插件

--- wzapp/client/widget/message/message.tpl
+++ wzapp/client/widget/message/message.tpl
@@ -1,1 +1,3 @@
+{% require "wzapp:static/message.js" %}
+
 <h1>Hello world!</h1>
--- yog/conf/fis-conf.js
+++ yog/conf/fis-conf.js
@@ -113,7 +113,10 @@ module.exports = function(sconfig) {
         });
 
         fis.media('prod').match('::package', {
-            packager: fis.plugin('map')
+            packager: fis.plugin('map', {
+                "/client/widget/build.js": "/client/widget/**.js",
+                "/client/widget/build.css": "/client/widget/**.scss"
+            })
         });
 
         fis.media('prod').match('*', {
@@ -130,5 +133,20 @@ module.exports = function(sconfig) {
                 receiver: 'http://127.0.0.1:8085/yog/upload'
             }
         });
+    }
+
+    /**
+     * 打包阶段插件接口
+     * @param  {Object} ret      一个包含处理后源码的结构
+     * @param  {Object} conf     一般不需要关心，自动打包配置文件
+     * @param  {Object} settings 插件配置属性
+     * @param  {Object} opt      命令行参数
+     * @return {undefined}
+     */
+    function replaceImg(ret, conf, settings, opt) {
+        // ret.src 所有的源码，结构是 {'<subpath>': <File 对象>}
+        // ret.ids 所有源码列表，结构是 {'<id>': <File 对象>}
+        // ret.map 如果是 spriter、postpackager 这时候已经能得到打包结果了，
+        //         可以修改静态资源列表或者其他
     }
 }
