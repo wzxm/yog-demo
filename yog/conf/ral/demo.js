c99f5c028ac7e4b44d38ef7f8da848a09e5b348d
245160801@qq.com <245160801@qq.com>
2017-03-08 15:03:21 +0800

    增加 ral 配置

--- yog/conf/ral/demo.js
+++ yog/conf/ral/demo.js
@@ -17,4 +17,29 @@ module.exports = {
   //         { host: '127.0.0.1', port: 8080}
   //     ]
   // }
+
+  'MAPAPI': {
+    protocol: 'http',
+    pack: 'querystring',
+    unpack: 'json',
+    encoding: 'utf-8',
+    // 负载均衡与超时重试配置
+    balance: 'roundrobin',
+    timeout: 500,
+    retry: 1,
+    // HTTP  协议特有配置
+    method: 'GET',
+    query: {},
+    path: '/statis/general',
+    headers: {
+      'x-client': 'ral'
+    },
+    // 后端地址配置
+    server: [
+      {
+        host: 'data.ppmoney.net',
+        port: 80
+      }
+    ]
+  }
 };
